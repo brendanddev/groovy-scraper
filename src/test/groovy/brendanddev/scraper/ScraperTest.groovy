60e7ff712be7ec741cd35064a9951df2705c6e3f
Brendan Dileo <dileob23@gmail.com>
2025-08-10 12:25:31 -0400

    Add basic test to scraper test

--- src/test/groovy/brendanddev/scraper/ScraperTest.groovy
+++ src/test/groovy/brendanddev/scraper/ScraperTest.groovy
@@ -5,7 +5,14 @@ import spock.lang.Unroll
 
 
 
-class ScraperTest {
+class ScraperTest extends Specification {
+
+    @Unroll
+    def "test USER_AGENT is not empty"() {
+        expect:
+        ScraperUtils.USER_AGENT?.trim()
+        ScraperUtils.USER_AGENT.length() > 0
+    }
 
 
 
