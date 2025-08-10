0a32df2059e055429ba11635c86f4a8404ca05ef
Brendan Dileo <dileob23@gmail.com>
2025-08-10 11:19:32 -0400

    Implement scrape quotes method

--- src/main/groovy/brendanddev/scraper/ScraperUtils.groovy
+++ src/main/groovy/brendanddev/scraper/ScraperUtils.groovy
@@ -1,6 +1,11 @@
 package brendanddev.scraper
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
 
 class ScraperUtils {
 
@@ -8,24 +13,42 @@ class ScraperUtils {
     static final int TIMEOUT = 5000
     static final int DELAY_BETWEEN_REQUESTS = 1000
 
+    /**
+     * Scrapes quotes from the website 'http://quotes.toscrape.com/'.
+     *
+     * This method connects to the site, parses the HTML to find the quote blocks,
+     * extracts the quote text, author, and tags, and prints them to the console.
+     * It includes a delay between requests to be respectful of the server load.
+     */
+    static void scrapeQuotes() {
+        try {
+            String url = "http://quotes.toscrape.com/"
 
+            Document doc = Jsoup.connect(url)
+                .userAgent(USER_AGENT)
+                .timeout(TIMEOUT)
+                .get()
+            
+            Elements quotes = doc.select("div.quote")
 
+            quotes.each { Element quote ->
 
+                String text = quote.select("span.text").text()
+                String author = quote.select("small.author").text()
+                Elements tags = quote.select("div.tags a.tag")
+                
+                println "Quote: ${text}"
+                println "Author: ${author}"
+                println "Tags: ${tags.collect { it.text() }.join(', ')}"
+                println "-" * 50
+                
+                Thread.sleep(100)
+            }
+        } catch (Exception e) {
+            println "Error during scraping: ${e.message}"
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 
     /**
      * Saves the given string content to a file at the specified path.
