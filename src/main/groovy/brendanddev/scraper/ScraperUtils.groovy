cd859c7ce4d767d761166322213388ddfbe70219
Brendan Dileo <dileob23@gmail.com>
2025-08-10 12:11:11 -0400

    Remove parse quotes method

--- src/main/groovy/brendanddev/scraper/ScraperUtils.groovy
+++ src/main/groovy/brendanddev/scraper/ScraperUtils.groovy
@@ -166,43 +166,6 @@ class ScraperUtils {
     }
 
     /**
-     * Scrapes quotes from the website 'http://quotes.toscrape.com/'.
-     *
-     * This method connects to the site, parses the HTML to find the quote blocks,
-     * extracts the quote text, author, and tags, and prints them to the console.
-     * It includes a delay between requests to be respectful of the server load.
-     */
-    static void scrapeQuotes() {
-        try {
-            String url = "http://quotes.toscrape.com/"
-
-            Document doc = Jsoup.connect(url)
-                .userAgent(USER_AGENT)
-                .timeout(TIMEOUT)
-                .get()
-            
-            Elements quotes = doc.select("div.quote")
-
-            quotes.each { Element quote ->
-
-                String text = quote.select("span.text").text()
-                String author = quote.select("small.author").text()
-                Elements tags = quote.select("div.tags a.tag")
-                
-                println "Quote: ${text}"
-                println "Author: ${author}"
-                println "Tags: ${tags.collect { it.text() }.join(', ')}"
-                println "-" * 50
-                
-                Thread.sleep(100)
-            }
-        } catch (Exception e) {
-            println "Error during scraping: ${e.message}"
-        }
-    }
-
-
-    /**
      * Saves the given string content to a file at the specified path.
      *
      * @param content The text content to write to the file.
