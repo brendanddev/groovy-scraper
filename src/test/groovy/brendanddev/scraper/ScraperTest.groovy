6a48da9f261fe7a08ed2a5bfdc44d6f35f6a8d54
Brendan Dileo <dileob23@gmail.com>
2025-08-10 12:28:41 -0400

    Update config contant tests

--- src/test/groovy/brendanddev/scraper/ScraperTest.groovy
+++ src/test/groovy/brendanddev/scraper/ScraperTest.groovy
@@ -4,9 +4,13 @@ import spock.lang.Specification
 import spock.lang.Unroll
 
 
-
+/**
+ * Defines unit tests for the ScraperUtils class to test functionality,
+ * ensure configurations are loaded, and validate utility methods.
+ */
 class ScraperTest extends Specification {
 
+    // Verifies the USER_AGENT config constant is set and not empty
     @Unroll
     def "test USER_AGENT is not empty"() {
         expect:
@@ -14,6 +18,17 @@ class ScraperTest extends Specification {
         ScraperUtils.USER_AGENT.length() > 0
     }
 
+    // Verifies the TIMEOUT config constant is a positive integer
+    def "test TIMEOUT is a positive integer"() {
+        expect:
+        ScraperUtils.TIMEOUT > 0
+    }
+
+    // Verifies the DELAY_BETWEEN_REQUESTS config constant is a positive integer
+    def "test DELAY_BETWEEN_REQUESTS is a positive integer"() {
+        expect:
+        ScraperUtils.DELAY_BETWEEN_REQUESTS > 0
+    }
 
 
 
