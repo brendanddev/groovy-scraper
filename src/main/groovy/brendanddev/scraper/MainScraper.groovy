f4ac7f7db6ec7bd7ea7e93f95961806eeb83ec8c
Brendan Dileo <dileob23@gmail.com>
2025-08-10 10:56:24 -0400

    Create ScraperUtils for utility logic

--- src/main/groovy/brendanddev/scraper/MainScraper.groovy
+++ src/main/groovy/brendanddev/scraper/MainScraper.groovy
@@ -1,4 +1,22 @@
 package brendanddev.scraper
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+
+
 class MainScraper {
+
+
+
+
+
+
+
+
+
+
 }
