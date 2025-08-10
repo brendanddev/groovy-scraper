4b008c06d1316493d0ee517d79c07f0b987beb71
Brendan Dileo <dileob23@gmail.com>
2025-08-10 11:19:42 -0400

    Update main class to test scraping

--- src/main/groovy/brendanddev/scraper/MainScraper.groovy
+++ src/main/groovy/brendanddev/scraper/MainScraper.groovy
@@ -11,12 +11,20 @@ import groovy.json.JsonSlurper
 class MainScraper {
 
 
+    static void main(String[] args) {
+        println "Starting the scraper..."
 
+        // Test scraping quotes
+        ScraperUtils.scrapeQuotes()
 
+        // Test checking robots.txt
+        ScraperUtils.checkRobotsTxt("http://quotes.toscrape.com")
 
+        // Test saving to file
+        String sampleContent = "Hello from scraping!"
+        ScraperUtils.saveToFile(sampleContent, "output.txt")
 
-
-
-
+        println "Scraping tests completed."
+    }
 
 }
