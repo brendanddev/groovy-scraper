ce1ecfe0705914ff29c011a5401d954930e0df86
Brendan Dileo <dileob23@gmail.com>
2025-08-10 13:29:51 -0400

    Move robots.txt test to integration tests class

--- src/test/groovy/brendanddev/scraper/ScraperIntegrationTest.groovy
+++ src/test/groovy/brendanddev/scraper/ScraperIntegrationTest.groovy
@@ -4,6 +4,24 @@ import spock.lang.Specification
 
 class ScraperIntegrationTest extends Specification {
 
+    // Tests the checkRobotsTxt method to ensure it can fetch and print robots.txt content
+    def "should successfully check robots.txt"() {
+        when: "checking robots.txt for educational site"
+        ScraperUtils.checkRobotsTxt("http://quotes.toscrape.com")
+        
+        then: "should complete without throwing exception"
+        notThrown(Exception)
+    }
+
+    // Tests the checkRobotsTxt method for a site without robots.txt
+    def "should handle non-existent robots.txt gracefully"() {
+        when: "checking robots.txt for site without one"
+        ScraperUtils.checkRobotsTxt("https://httpbin.org")
+        
+        then: "should handle gracefully"
+        notThrown(Exception)
+    }
+
 
 
 }
--- src/test/groovy/brendanddev/scraper/ScraperTest.groovy
+++ src/test/groovy/brendanddev/scraper/ScraperTest.groovy
@@ -86,24 +86,6 @@ class ScraperTest extends Specification {
         notThrown(Exception)
     }
 
-    // Tests the checkRobotsTxt method to ensure it can fetch and print robots.txt content
-    def "should successfully check robots.txt"() {
-        when: "checking robots.txt for educational site"
-        ScraperUtils.checkRobotsTxt("http://quotes.toscrape.com")
-        
-        then: "should complete without throwing exception"
-        notThrown(Exception)
-    }
-
-    // Tests the checkRobotsTxt method for a site without robots.txt
-    def "should handle non-existent robots.txt gracefully"() {
-        when: "checking robots.txt for site without one"
-        ScraperUtils.checkRobotsTxt("https://httpbin.org")
-        
-        then: "should handle gracefully"
-        notThrown(Exception)
-    }
-
 
 
 
