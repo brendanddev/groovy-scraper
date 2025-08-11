83eee579e2052093463865dcd4d6bba722875405
Brendan Dileo <dileob23@gmail.com>
2025-08-11 10:25:39 -0400

    Move banner logic

--- src/main/groovy/brendanddev/scraper/CliBanner.groovy
+++ src/main/groovy/brendanddev/scraper/CliBanner.groovy
@@ -1,16 +1,22 @@
 package brendanddev.scraper
 
+/**
+ * Defines a utility class for displaying the applications startup banner.
+ */
 class CliBanner {
 
-    
+    /**
+     * Prints the application banner to be shown at startup
+     */
+    static void printBanner() {
+        println """
+            ╔══════════════════════════════════════════════════════════════╗
+            ║                           Web Scraper                        ║
+            ╠══════════════════════════════════════════════════════════════╣                    
+            ║                    by brendanddev                            ║
+            ╠══════════════════════════════════════════════════════════════╣
+            ╚══════════════════════════════════════════════════════════════╝
+        """
+    }
 
-
-    
-
-
-
-
-
-
-    
 }
